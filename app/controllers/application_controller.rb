3889148b8d150af948bfc878e87e8be6d5bdb847
vagrant <vagrant@localhost.localdomain>
2020-07-04 11:53:35 +0000

    [Finish]rspecクリア

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
+    flash[:notice]="Welcome! You have signed up successfully."
     user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
+    flash[:notice]="Signed out successfully."
     root_path
   end
 
