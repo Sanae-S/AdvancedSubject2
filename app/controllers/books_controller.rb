ec9f1abeb41650a913c8c79f9c19397359f23349
vagrant <vagrant@localhost.localdomain>
2020-07-04 10:02:28 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
 
+before_action :correct_user, only: [:edit, :update, :destroy]
+
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def index
@@ -9,13 +13,15 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -30,20 +36,26 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
+  def correct_user
+    @book = Book.find(params[:id])
+    if current_user != @book.user
+       redirect_to books_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
 
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-  validates :name, length: {maximum: 20, minimum: 2}
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}
+  validates :introduction, presence: true
 end
