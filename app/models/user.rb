43d5c4db50c7178b9dfe2b8e6e93700c82b44370
vagrant <vagrant@localhost.localdomain>
2020-07-05 19:16:47 +0000

    [Fix]FavoriteModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   attachment :profile_image, destroy: false
-
+  has_many :favorites, dependent: :destroy
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction,length: { maximum: 50 }
