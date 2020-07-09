60d7f76fccea3f47b51f3f392ca5aa8ce12642bc
vagrant <vagrant@localhost.localdomain>
2020-07-09 06:25:02 +0000

    [Update]UserModel フォローされているユーザーを取り出す（user.follwersをできるようにする）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,17 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
+    def following?(other_user)
+      following_relationships.find_by(following_id: other_user.id)
+    end
+
+    def follow!(other_user)
+      following_relationships.create!(following_id: other_user.id)
+    end
+
+    def unfollow!(other_user)
+      following_relationships.find_by(following_id: other_user.id).destroy
+    end
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction,length: { maximum: 50 }
