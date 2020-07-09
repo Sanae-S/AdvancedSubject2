a6620c7085001699eb325b610233cb17a572f9db
vagrant <vagrant@localhost.localdomain>
2020-07-09 09:08:45 +0000

    [Finish]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   	member do
      get :following, :followers
     end
-  	resources :relationships, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
+
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
